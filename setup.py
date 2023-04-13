bbad3873217931a8b126aa8ab1d4991f1f787367
Hang Wei <30917686+special-PPT@users.noreply.github.com>
2023-04-13 18:58:47 -0400

    change name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="charbot_app",
+    name="chatbot_app",
     version="0.1",
     packages=find_packages(),
     include_package_data=True,
